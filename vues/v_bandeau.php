86ad92e804963001f1ae302b3f851dccab675da1
CotCodex <laura.ecole.92@gmail.com>
2024-10-26 15:57:54 +0200

    page profile

---
+++ vues/v_bandeau.php
@@ -0,0 +1,20 @@
+<header>
+   <img alt="Logo" height="40" src="https://storage.googleapis.com/a1aa/image/0WOLHzZ8QvJfLaN7Lsu20IaDgBDB7Y6Ne70uXWi75lOPToqTA.jpg" width="40"/>
+   <nav>
+    <a href="index.php?uc=profil">
+     Profil
+    </a>
+    <a href="index.php?uc=formation">
+     Formation
+    </a>
+    <a href="index.php?uc=projets">
+     Projets
+    </a>
+    <a href="index.php?uc=veille">
+     Veille
+    </a>
+    <a href="index.php?uc=contact">
+     Contact
+    </a>
+   </nav>
+  </header>
